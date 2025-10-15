3e03b05a3e6a522ab62cee215a52d2579c86e1b8
Ro7 <123015624+Rbrtpv@users.noreply.github.com>
2025-10-14 22:04:08 -0400

    feat(pages): add new Editor page
    This commit introduces the new `Editor` page component. This page will serve as the primary user interface for creating and managing activities.
    
    The initial implementation includes a heading and integrates the `NewActivity` component, establishing the basic layout for the editor view.

---
+++ src/pages/Editor.tsx
@@ -0,0 +1,12 @@
+import NewActivity from "../components/NewActivity";
+
+function Editor() {
+  return (
+    <>
+      <h2>Editor</h2>
+      <NewActivity />
+    </>
+  );
+}
+
+export default Editor;
